eba64d3f801380c7b8d056a71bf3d550e5ff6010
Akiff Manji <akiff.manji@gmail.com>
2021-09-18 10:02:48 -0700

    Adds QR code scanner
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- src/views/Scanner.tsx
+++ src/views/Scanner.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Vibration, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 export const Scanner = () => {
   return (
-    <View>
-      <Text>QR Code Scanner goes here</Text>
+    <View style={styles.container}>
+      <RNCamera
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        captureAudio={false}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+        onBarCodeRead={e => {
+          Vibration.vibrate();
+          console.log(e);
+          // TODO: Handle the code scan
+        }}>
+        <View style={styles.window} />
+      </RNCamera>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  window: {
+    height: 250,
+    width: 250,
+    padding: 100,
+    backgroundColor: 'rgba(255,255,255,0.3)',
+    borderRadius: 30,
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+});
