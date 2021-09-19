99cb08f26aae1c307568a64545e9a8c6f55ec68e
Akiff Manji <akiff.manji@gmail.com>
2021-09-19 13:19:11 -0700

    Retrieve encoded url from uploaded image
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- src/views/Scanner.tsx
+++ src/views/Scanner.tsx
@@ -1,10 +1,73 @@
 import React, { useState } from 'react';
-import { StyleSheet, Vibration, View } from 'react-native';
+import {
+  PermissionsAndroid,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Vibration,
+  View,
+} from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import EncryptedStorage from 'react-native-encrypted-storage';
+import { launchImageLibrary } from 'react-native-image-picker';
+import RNQRGenerator from 'rn-qr-generator';
 
 export const Scanner = ({ navigation }) => {
   const [active, setActive] = useState(true);
+
+  const storeQrCodeUrl = async (url: string) => {
+    try {
+      let shcVaccinations = [];
+      const storedShcVaccinations = await EncryptedStorage.getItem(
+        'shc_vaccinations',
+      );
+      if (storedShcVaccinations) {
+        shcVaccinations = JSON.parse(storedShcVaccinations);
+      }
+      shcVaccinations.push({
+        url,
+        date: Date.now(),
+      });
+      await EncryptedStorage.setItem(
+        'shc_vaccinations',
+        JSON.stringify(shcVaccinations),
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  async function hasAndroidPermission() {
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+
+    const hasPermission = await PermissionsAndroid.check(permission);
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(permission);
+    return status === 'granted';
+  }
+
+  async function uploadImage() {
+    if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
+      return;
+    }
+
+    try {
+      setActive(false);
+      launchImageLibrary({ mediaType: 'photo', base64: true }, async image => {
+        const res = await RNQRGenerator.detect({ uri: image?.assets[0]?.uri });
+        if (res.values.length) {
+          await Promise.all(res.values.map(storeQrCodeUrl));
+          navigation.navigate('Credentials');
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -28,6 +91,16 @@ export const Scanner = ({ navigation }) => {
             navigation.navigate('Credentials');
           }}>
           <View style={styles.window} />
+          <View
+            style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+            <TouchableOpacity
+              style={styles.upload}
+              onPress={() => {
+                uploadImage();
+              }}>
+              <Text style={{ fontSize: 18 }}>Upload</Text>
+            </TouchableOpacity>
+          </View>
         </RNCamera>
       )}
     </View>
@@ -54,26 +127,12 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'white',
   },
+  upload: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 16,
+    alignSelf: 'center',
+    margin: 32,
+  },
 });
-
-const storeQrCodeUrl = async (url: string) => {
-  try {
-    let shcVaccinations = [];
-    const storedShcVaccinations = await EncryptedStorage.getItem(
-      'shc_vaccinations',
-    );
-    if (storedShcVaccinations) {
-      shcVaccinations = JSON.parse(storedShcVaccinations);
-    }
-    shcVaccinations.push({
-      url,
-      date: Date.now(),
-    });
-    await EncryptedStorage.setItem(
-      'shc_vaccinations',
-      JSON.stringify(shcVaccinations),
-    );
-  } catch (error) {
-    console.error(error);
-  }
-};
