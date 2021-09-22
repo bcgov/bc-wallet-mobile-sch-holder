1b2677ec4cc7ba8055c4da3b1cacbfabc3b15d70
Akiff Manji <akiff.manji@gmail.com>
2021-09-22 12:21:11 -0700

    Adds prelimiary theming
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- src/components/credential/CredentialTabs.tsx
+++ src/components/credential/CredentialTabs.tsx
@@ -1,22 +1,47 @@
+import {useTheme} from '@emotion/react';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
+import {AppTheme} from '../../../App';
 import {Credentials} from '../../views/credential/Credentials';
 import {Settings} from '../../views/Settings';
 
 const Tab = createBottomTabNavigator();
 
 export const CredentialTabs = () => {
+  const theme = useTheme() as AppTheme;
   return (
-    <Tab.Navigator initialRouteName="Credentials">
+    <Tab.Navigator
+      initialRouteName="Credentials"
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: theme.colors.white,
+        },
+        tabBarActiveTintColor: theme.colors.headerBlue,
+        tabBarInactiveTintColor: theme.colors.inputGray,
+      }}>
       <Tab.Screen
         name="Credentials"
         component={Credentials}
-        options={{headerShown: false, tabBarLabel: 'Your Wallet'}}
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Cards',
+          tabBarIcon: ({color, size}) => (
+            <FontAwesomeIcon icon="id-card" size={size || 24} color={color} />
+          ),
+          tabBarLabelStyle: {fontSize: 12},
+        }}
       />
       <Tab.Screen
         name="Settings"
         component={Settings}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <FontAwesomeIcon icon="cog" size={size || 24} color={color} />
+          ),
+          tabBarLabelStyle: {fontSize: 12},
+        }}
       />
     </Tab.Navigator>
   );
